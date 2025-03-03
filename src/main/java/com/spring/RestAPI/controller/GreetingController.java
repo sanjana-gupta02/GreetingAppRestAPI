b58e4e3e2835b6e09353688c79990966a08a3a48
Sanjana Gupta <sanjanagupta1502@gmail.com>
2025-03-03 15:40:07 +0530

    Added initial files in Feature_UC3

--- src/main/java/com/spring/RestAPI/controller/GreetingController.java
+++ src/main/java/com/spring/RestAPI/controller/GreetingController.java
@@ -3,11 +3,10 @@ package com.spring.RestAPI.controller;
 import com.spring.RestAPI.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/greeting")
 public class GreetingController {
 
     private final GreetingService greetingService;
@@ -17,8 +16,10 @@ public class GreetingController {
         this.greetingService = greetingService;
     }
 
-    @GetMapping
-    public String getGreeting() {
-        return greetingService.getGreetingMessage();
+    @GetMapping("/greeting")
+    public String getGreeting(
+            @RequestParam(required = false) String firstName,
+            @RequestParam(required = false) String lastName) {
+        return greetingService.getGreetingMessage(firstName, lastName);
     }
 }
