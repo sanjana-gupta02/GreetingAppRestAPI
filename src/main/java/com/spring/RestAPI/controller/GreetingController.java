56a6d24555102fa7f7a54a6a5893e64b35ded2d0
Sanjana Gupta <sanjanagupta1502@gmail.com>
2025-03-03 15:04:24 +0530

    Added initial files in Feature_UC2

--- src/main/java/com/spring/RestAPI/controller/GreetingController.java
+++ src/main/java/com/spring/RestAPI/controller/GreetingController.java
@@ -1,30 +1,24 @@
 package com.spring.RestAPI.controller;
 
-import org.springframework.web.bind.annotation.*;
+import com.spring.RestAPI.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
 
+    private final GreetingService greetingService;
+
+    @Autowired
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     @GetMapping
-    public GreetingResponse getGreeting() {
-        return new GreetingResponse("Hello, this is a GET request!", null);
+    public String getGreeting() {
+        return greetingService.getGreetingMessage();
     }
-
-    @PostMapping
-    public GreetingResponse postGreeting(@RequestBody String name) {
-        return new GreetingResponse("Hello, this is a POST request!", name);
-    }
-
-    @PutMapping
-    public GreetingResponse putGreeting(@RequestBody String name) {
-        return new GreetingResponse("Hello, this is a PUT request!", name);
-    }
-
-    @DeleteMapping
-    public GreetingResponse deleteGreeting() {
-        return new GreetingResponse("Hello, this is a DELETE request!", null);
-    }
-
-    public record GreetingResponse(String message, String name) {}
 }
